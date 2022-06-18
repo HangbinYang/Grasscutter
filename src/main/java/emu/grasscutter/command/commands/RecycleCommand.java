237b1c7037f913d620ac065b18c948389ed2142e
Hangbin <yanghangbin@gmail.com>
2022-06-18 14:16:23 +0800

    Add Command: Recyle

---
+++ src/main/java/emu/grasscutter/command/commands/RecycleCommand.java
@@ -0,0 +1,81 @@
+package emu.grasscutter.command.commands;
+
+import emu.grasscutter.Grasscutter;
+import emu.grasscutter.command.Command;
+import emu.grasscutter.command.CommandHandler;
+import emu.grasscutter.game.inventory.GameItem;
+import emu.grasscutter.game.inventory.Inventory;
+import emu.grasscutter.game.inventory.ItemType;
+import emu.grasscutter.game.player.Player;
+
+import java.util.List;
+
+import static emu.grasscutter.utils.Language.translate;
+
+@Command(label = "recycle", usage = "recycle <wp|art>",
+        description = "commands.recycle.description",
+        aliases = {"recycle"}, permission = "player.recycle", permissionTargeted = "player.recycle.others")
+
+public final class RecycleCommand implements CommandHandler {
+
+    @Override
+    public void execute(Player sender, Player targetPlayer, List<String> args) {
+        if (args.size() < 1) {
+            CommandHandler.sendMessage(sender, translate(sender, "commands.recycle.command_usage"));
+            return;
+        }
+        Inventory playerInventory = targetPlayer.getInventory();
+        List<GameItem> toDelete = null;
+        List<GameItem> toGive = new LinkedList<>();
+        
+        switch (args.get(0)) {
+            case "wp" -> {
+            	toDelete = playerInventory.getItems().values().stream()
+                    .filter(item -> item.getItemType() == ItemType.ITEM_WEAPON)
+                    .filter(item -> !item.isLocked() && !item.isEquipped())
+                    .toList();
+                for (GameItem item: toDelete) {
+                    ItemData itemData = GameData.getItemDataMap().get(104012);
+                    GameItem newItem = new GameItem(itemData);
+                    item.setLevel(1);
+                    if (item.getPromoteLevel() > 5) {
+                        item.setCount(50);
+                    } else if (item.getPromoteLevel() >= 4) {
+                        item.setCount(30);
+                    } else {
+                        item.setCount(10);
+                    }
+                    toGive.add(item);
+                }
+                targetPlayer.getInventory().addItems(toGive, ActionReason.SubfieldDrop);
+                CommandHandler.sendMessage(sender, translate(sender, "commands.recycle.weapons", targetPlayer.getNickname()));
+            }
+            case "art" -> {
+            	toDelete = playerInventory.getItems().values().stream()
+                    .filter(item -> item.getItemType() == ItemType.ITEM_RELIQUARY)
+                    .filter(item -> item.getLevel() == 1 && item.getExp() == 0)
+                    .filter(item -> !item.isLocked() && !item.isEquipped())
+                    .toList();
+                for (GameItem item: toDelete) {
+                    ItemData itemData = GameData.getItemDataMap().get(105003);
+                    GameItem newItem = new GameItem(itemData);
+                    item.setLevel(1);
+                    if (item.getPromoteLevel() > 5) {
+                        item.setCount(50);
+                    } else if (item.getPromoteLevel() >= 4) {
+                        item.setCount(30);
+                    } else {
+                        item.setCount(10);
+                    }
+                    toGive.add(item);
+                }
+                targetPlayer.getInventory().addItems(toGive, ActionReason.SubfieldDrop);
+                CommandHandler.sendMessage(sender, translate(sender, "commands.recycle.artifacts", targetPlayer.getNickname()));
+            }
+        }
+        
+        if (toDelete != null) {
+        	playerInventory.removeItems(toDelete);
+        }
+    }
+}
